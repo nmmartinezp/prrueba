b2c89987edcb2494685173201b02fe0fd1a1d607
nmmartinezp <nikimartin56@gmail.com>
2024-03-03 19:20:42 -0400

    main

---
+++ main.js
@@ -0,0 +1,19 @@
+function main(){
+    let a = new Complex("hola");
+    console.log(a.dev());
+    console.log(a.dev2());
+    console.log(a.dev3());
+    console.log(a.dev4());
+}
+
+class Complex{
+    constructor(real){
+        this.real = real;
+    }
+
+    dev(){
+        return this.real;
+    }
+}
+
+main();
